a61542bbc6a1ea3668306c7323969fa70370af89
Moch Syafiq Wahyu Anggara <56503635+ompiko@users.noreply.github.com>
2023-01-23 22:38:21 +0700

    initial commit

--- README.md
+++ README.md
@@ -9,6 +9,12 @@
 
 ## About Laravel
 
+Framework: Laravel
+PHP version for this apps is 8.2.1
+Database use MySQL
+username: admin@admin.com
+password: !23Qweasd
+
 Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
 
 - [Simple, fast routing engine](https://laravel.com/docs/routing).
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = User::create([
+            'name'  =>  'admin',
+            'email' =>  'admin@admin.com',
+            'password'  =>  Hash::make('12345678'),
+            'is_admin'  =>  1,
+            ]);
+            UserDetail::create([
+            'user_id'   =>  $user->id,
+        ]);
+    }
+}
